b5dc5189f12de433d5501fcb6415f4cd2317180e
Ethan Meltzer <captainbanaynays@gmail.com>
2022-12-22 01:32:48 -0500

    Fixed formatting in solution file.

--- 01_01/src/main.rs
+++ 01_01/src/main.rs
@@ -15,30 +15,30 @@ fn main() {
     let mut max_sum = 0;
     let mut input_line = String::new();
     let mut read_ret;
-    
-    // Read in lines from input. Parse as integers, panic if error. Add to single sum.
-    
-    loop {
-    
-       read_ret = io::stdin().read_line(&mut input_line).unwrap();
-       input_line = strip_trailing_newline(&input_line).to_string();
 
-       if read_ret == 0 {
-           break;
-       }
-       else if input_line == "" {
-           if single_sum > max_sum {
-               max_sum = single_sum;
-           }
-           single_sum = 0;
-       }
-       else {
-           single_sum += match input_line.parse::<i32>() {
-               Ok(n) => n,
-               Err(err) => panic!("Error reading the file: {err}"),
-           }
-       }
-       input_line = "".to_string();
+    // Read in lines from input. Parse as integers, panic if error. Add to single sum.
+
+    loop {
+
+        read_ret = io::stdin().read_line(&mut input_line).unwrap();
+        input_line = strip_trailing_newline(&input_line).to_string();
+
+        if read_ret == 0 {
+            break;
+        }
+        else if input_line == "" {
+            if single_sum > max_sum {
+                max_sum = single_sum;
+            }
+            single_sum = 0;
+        }
+        else {
+            single_sum += match input_line.parse::<i32>() {
+                Ok(n) => n,
+                Err(err) => panic!("Error reading the file: {err}"),
+            }
+        }
+        input_line = "".to_string();
     }
 
     println!("{max_sum}");
