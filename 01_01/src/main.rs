edde68d34fc1e6753ccbac0ca7488105a95c2d7b
Ethan Meltzer <captainbanaynays@ethans-air.myfiosgateway.com>
2022-12-22 01:26:42 -0500

    Part 1 of day 1 is done!

---
+++ 01_01/src/main.rs
@@ -0,0 +1,46 @@
+// Ethan Meltzer
+// 12/21/2022
+
+use std::io;
+
+fn strip_trailing_newline(input: &str) -> &str {
+    input
+        .strip_suffix("\r\n")
+        .or(input.strip_suffix("\n"))
+        .unwrap_or(input)
+}
+
+fn main() {
+    let mut single_sum = 0;
+    let mut max_sum = 0;
+    let mut input_line = String::new();
+    let mut read_ret;
+    
+    // Read in lines from input. Parse as integers, panic if error. Add to single sum.
+    
+    loop {
+    
+       read_ret = io::stdin().read_line(&mut input_line).unwrap();
+       input_line = strip_trailing_newline(&input_line).to_string();
+
+       if read_ret == 0 {
+           break;
+       }
+       else if input_line == "" {
+           if single_sum > max_sum {
+               max_sum = single_sum;
+           }
+           single_sum = 0;
+       }
+       else {
+           single_sum += match input_line.parse::<i32>() {
+               Ok(n) => n,
+               Err(err) => panic!("Error reading the file: {err}"),
+           }
+       }
+       input_line = "".to_string();
+    }
+
+    println!("{max_sum}");
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2022
+
+Listed in this directory are my solutions for the advent of code event for 2022.
+I'm a little late to the party, but that's better than never!
+I will be doing my best to code all of my solutions in Rust, in an attempt to learn the language.
+What fun! Such Merry-making! Wheeee! ❄️ ❄️ ❄️ ❄️  
