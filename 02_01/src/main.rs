fd29e07468de751dfdd31f1f21c7c244a9f440d0
Ethan Meltzer <captainbanaynays@gmail.com>
2022-12-25 01:04:00 -0500

    Added Day 2 stuff hopefully

---
+++ 02_01/src/main.rs
@@ -0,0 +1,12 @@
+// Ethan Meltzer
+// 12/22/2022
+
+use std::io::stdin;
+
+fn main() {
+    let mut total_sum = 0;
+    let mut single_sum = 0;
+    let mut line = String::new();
+
+    stdin().read_line(buf)
+}
