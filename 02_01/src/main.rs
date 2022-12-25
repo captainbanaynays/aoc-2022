c7f4ba85e244630e42cda334057dd03ec0a8e738
Ethan Meltzer <captainbanaynays@gmail.com>
2022-12-25 02:18:26 -0500

    Ok day 2 still broken
    But I'm pretty sure that I know why! Stay tuned.

--- 02_01/src/main.rs
+++ 02_01/src/main.rs
@@ -3,10 +3,24 @@
 
 use std::io::stdin;
 
+// TODO: I fucked up the order of who goes first and who goes second? IDK need to check tomorrow.
 fn main() {
-    let mut total_sum = 0;
-    let mut single_sum = 0;
+    let mut total_sum:i32 = 0;
     let mut line = String::new();
 
-    stdin().read_line(buf)
+    while stdin().read_line(&mut line).unwrap() != 0 {
+        let p1:u8 = (line.chars().nth(0).unwrap() as u8) - ('A' as u8);
+        let p2:u8 = (line.chars().nth(2).unwrap() as u8) - ('X' as u8);
+
+        total_sum += p1 as i32 + match p2 {
+            0 => 3*((p1 + 1) % 3),
+            1 => 3*((p1 + 0) % 3),
+            2 => 3*((p1 + 2) % 3),
+            _ => panic!("Result is wrong!"),
+        } as i32;
+        
+        line = "".to_string();
+    };
+
+    println!("{total_sum}");
 }
