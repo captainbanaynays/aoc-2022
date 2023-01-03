a1536314038fd22d971963d6450ea48e28bde633
Ethan Meltzer <captainbanaynays@gmail.com>
2023-01-03 18:39:23 -0500

    Day 4 Part 2 Done!

---
+++ 04_02/src/main.rs
@@ -0,0 +1,31 @@
+// Ethan Meltzer
+// 01/03/2023
+
+use std::io::stdin;
+use std::vec::Vec;
+
+fn main() {
+    let mut total:i32 = 0;
+    let mut line:String = String::new();
+
+    while stdin().read_line(&mut line).unwrap() != 0 {
+        line = line.trim().to_string();
+        let slices = line.split(&['-', ','][..]);
+        let mut nums:Vec<i32> = Vec::new();
+
+        for i in slices {
+            nums.push(i.parse::<i32>().unwrap());
+        }
+
+        assert!(nums.len() == 4);
+        assert!(nums[1] >= nums[0] && nums[3] >= nums[2]);
+
+        if nums[0] <= nums[3] && nums[1] >= nums[2] {
+            total += 1;
+        }
+
+        line = "".to_string();
+    }
+    println!("{total}");
+}
+
